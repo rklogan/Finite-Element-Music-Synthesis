ca2eb930f3ce908098d3befb6a3243dd3579daa0
rklogan <ryan.k.logan@gmail.com>
2019-11-14 16:49:59 -0500

    fixed initialization

--- q1.py
+++ q1.py
@@ -1,6 +1,9 @@
 import sys
 import numpy
 
+def process_entry(x,y,grid):
+    pass
+
 #basic parameters
 grid_size = 4
 center = int(grid_size / 2)
@@ -10,10 +13,14 @@ num_fes = grid_size * grid_size
 T = sys.argv[1]
 
 #initialize the array
-drum_skin = numpy.zeros((grid_size, grid_size), dtype=numpy.float)
-drum_skin[center][center] = 1.0
+drum_skin = numpy.zeros((grid_size, grid_size), dtype=(numpy.float,3))
+drum_skin[center][center] = (1.0, 0.0, 0.0)
 
 
-
+for i in range(4):
+    for j in range(4):
+        for k in range(3):
+            print(drum_skin[i][j][k])
+            
 
 
