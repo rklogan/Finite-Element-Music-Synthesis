e2cb634fa6ed08644c6ee76a3a4a1abab512661c
rklogan <ryan.k.logan@gmail.com>
2019-11-27 16:10:24 -0500

    cleanup/refactoring

--- q2.py
+++ q2.py
@@ -11,7 +11,7 @@ rho = 0.5
 G = 0.75
 
 @cuda.jit
-def iterate(grid, output_grid, width, corner_barrier):
+def iterate(grid, width, corner_barrier, num_iterations):
     index = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
     if index >= width * width: return
 
@@ -20,60 +20,71 @@ def iterate(grid, output_grid, width, corner_barrier):
 
     thread_is_boundary = row == 0 or col == 0 or row == width-1 or col == width-1
 
-    #process interior
-    if not thread_is_boundary:
-        grid[row][col][0] = grid[row-1][col][1]
-        grid[row][col][0] += grid[row+1][col][1]
-        grid[row][col][0] += grid[row][col-1][1]
-        grid[row][col][0] += grid[row][col+1][1]
-        grid[row][col][0] -= 4 * grid[row][col][1]
-        grid[row][col][0] *= rho
-        grid[row][col][0] += 2 * grid[row][col][1]
-        grid[row][col][0] -= (1 - eta) * grid[row][col][2]
-        grid[row][col][0] /= (1 + eta)
 
-    #print('interior done')
-    #boundary nodes need to wait here for the interior to be written
-    cuda.syncthreads()
 
-    thread_is_corner = (row == 0 and col == 0) or (row == 0 and col == width-1) or (row == width-1 and col ==0) or (row == width-1 and col == width-1)
 
-    #boundary nodes can now start to work
-    if thread_is_boundary and not thread_is_corner:
-        if row == 0:
-            grid[0][col][0] = G * grid[1][col][0]
-        elif row == width - 1:
-            grid[width-1][col][0] = G * grid[width-2][col][0]
-        elif col == 0:
-            grid[row][0][0] = G * grid[row][1][0]
-        elif col == width - 1:
-            grid[row][width-1][0] = G * grid[row][width-2][0]
+    iteration = 0
+    while iteration < num_iterations:
+        #process interior
+        if not thread_is_boundary:
+            grid[row][col][0] = grid[row-1][col][1]
+            grid[row][col][0] += grid[row+1][col][1]
+            grid[row][col][0] += grid[row][col-1][1]
+            grid[row][col][0] += grid[row][col+1][1]
+            grid[row][col][0] -= 4 * grid[row][col][1]
+            grid[row][col][0] *= rho
+            grid[row][col][0] += 2 * grid[row][col][1]
+            grid[row][col][0] -= (1 - eta) * grid[row][col][2]
+            grid[row][col][0] /= (1 + eta)
 
-    if (row == 1 and col == 0) or (row == width-2 and col == 0) or (row == 0 and col == width-2) or (row == width-1 and col == width-2):
-        cuda.atomic.add(corner_barrier, 0, -1)
-    
-    #print(corner_barrier[0])
-            
-    if thread_is_corner:
-        #corners will busy-wait until their input data has been written
-        i = 0
-        while(corner_barrier[0] != 0):
-            i = (i + 1) %1024        
+        #print('interior done')
+        #boundary nodes need to wait here for the interior to be written
+        cuda.syncthreads()
 
-        if row == 0 and col ==0:
-            grid[0][0][0] = G * grid[1][0][0]
-        elif row == width-1 and col == 0:
-            grid[width-1][0][0] = G * grid[width-2][0][0]
-        elif row == 0 and col == width-1:
-            grid[0][width-1][0] = G * grid[0][width-2][0]
-        elif row == width-1 and col == width-1:
-            grid[width-1][width-1][0] = G * grid[width-1][width-2][0]
+        thread_is_corner = (row == 0 and col == 0) or (row == 0 and col == width-1) or (row == width-1 and col ==0) or (row == width-1 and col == width-1)
 
-    #print('all done')
-    #all threads can now propogate their values back
-    output_grid[row][col][2] = grid[row][col][1]
-    output_grid[row][col][1] = grid[row][col][0]
-    output_grid[row][col][0] = grid[row][col][0]
+        #boundary nodes can now start to work
+        if thread_is_boundary and not thread_is_corner:
+            if row == 0:
+                grid[0][col][0] = G * grid[1][col][0]
+            elif row == width - 1:
+                grid[width-1][col][0] = G * grid[width-2][col][0]
+            elif col == 0:
+                grid[row][0][0] = G * grid[row][1][0]
+            elif col == width - 1:
+                grid[row][width-1][0] = G * grid[row][width-2][0]
+
+        if (row == 1 and col == 0) or (row == width-2 and col == 0) or (row == 0 and col == width-2) or (row == width-1 and col == width-2):
+            cuda.atomic.add(corner_barrier, 0, -1)
+        
+        if thread_is_corner:
+            #corners will busy-wait until their input data has been written
+            while(corner_barrier[0] != 0):
+                pass
+            cuda.atomic.add(corner_barrier,0,-1)        
+
+            if row == 0 and col ==0:
+                grid[0][0][0] = G * grid[1][0][0]
+            elif row == width-1 and col == 0:
+                grid[width-1][0][0] = G * grid[width-2][0][0]
+            elif row == 0 and col == width-1:
+                grid[0][width-1][0] = G * grid[0][width-2][0]
+            elif row == width-1 and col == width-1:
+                grid[width-1][width-1][0] = G * grid[width-1][width-2][0]
+
+        #all threads can now propogate their values back
+        grid[row][col][2] = grid[row][col][1]
+        grid[row][col][1] = grid[row][col][0]
+        grid[row][col][0] = grid[row][col][0]
+
+        if row == width // 2 and col == width // 2:
+            print(grid[row][col][0])
+            while(cuda.atomic.compare_and_swap(corner_barrier, -4, 4) == 4):
+                pass
+
+        cuda.syncthreads()
+
+        iteration += 1
 
 
 if __name__ == "__main__":
@@ -86,16 +97,12 @@ if __name__ == "__main__":
     if len(sys.argv) >= 2:
         num_iterations = int(sys.argv[1])
 
-    for i in range(num_iterations):
-        barrier = np.array([4])
-        barrier_d = cuda.to_device(barrier)
-        grid_d = cuda.to_device(grid)
-        output_grid_d = cuda.device_array_like(grid)
+    barrier = np.array([4])
+    barrier_d = cuda.to_device(barrier)
+    grid_d = cuda.to_device(grid)
 
-        iterate[1,grid_size * grid_size](grid_d, output_grid_d, np.int(grid_size), barrier_d)
+    iterate[1,grid_size * grid_size](grid_d, np.int(grid_size), barrier_d, np.int(num_iterations))
 
-        grid = output_grid_d.copy_to_host()
-        print(grid[grid_size//2][grid_size//2][0])
 
     
     
