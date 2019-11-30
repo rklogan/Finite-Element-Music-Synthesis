81eab2f8ae23b3c689994386846c6bb4cf041988
rklogan <ryan.k.logan@gmail.com>
2019-11-30 14:55:10 -0500

    redevelopped. Finished

--- q2.py
+++ q2.py
@@ -1,110 +1,112 @@
-import sys
+""" Question 2: Cell by cell Parallelization """
 import numpy as np
 import numba
 from numba import cuda
-import q1
+import sys
 
 #define constants
 grid_size = 4
 eta = 0.0002
 rho = 0.5
 G = 0.75
+num_threads = 16
 
 @cuda.jit
-def iterate(grid, width, corner_barrier, num_iterations):
-    index = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
-    if index >= width * width: return
+def process_interior(grid, output, width):
+  index = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
+  if index >= (width - 2) * (width - 2): return
 
-    row = index // width
-    col = index % width
+  row = (index // (width-2)) + 1
+  col = (index % (width-2)) + 1
 
-    thread_is_boundary = row == 0 or col == 0 or row == width-1 or col == width-1
+  grid[row][col][0] = grid[row-1][col][1]
+  grid[row][col][0] += grid[row+1][col][1]
+  grid[row][col][0] += grid[row][col-1][1]
+  grid[row][col][0] += grid[row][col+1][1]
+  grid[row][col][0] -= 4 * grid[row][col][1]
+  grid[row][col][0] *= rho
+  grid[row][col][0] += 2 * grid[row][col][1]
+  grid[row][col][0] -= (1 - eta) * grid[row][col][2]
+  grid[row][col][0] /= (1 + eta)
 
+  output[0] = grid[width // 2][width // 2][0]
 
+@cuda.jit
+def process_edges(grid, width):
+  index = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
+  if index >= (width - 2) * 4: return
+  
+  if index < (width - 2):
+    grid[0][index+1][0] = G * grid[1][index+1][0]
+  elif index < 2 * (width - 2):
+    grid[width-1][(index % (width-2))+1][0] = G * grid[width -2][(index % (width-2))+1][0]
+  elif index < 3 * (width - 2):
+    grid[(index % (width-2))+1][0][0] = G * grid[(index % (width-2))+1][1][0]
+  else:
+    grid[(index % (width-2))+1][width-1][0] = G * grid[(index % (width-2))+1][width-2][0]
 
+@cuda.jit
+def process_corners(grid, width):
+  index = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
+  if index >= 4: return
 
-    iteration = 0
-    while iteration < num_iterations:
-        #process interior
-        if not thread_is_boundary:
-            grid[row][col][0] = grid[row-1][col][1]
-            grid[row][col][0] += grid[row+1][col][1]
-            grid[row][col][0] += grid[row][col-1][1]
-            grid[row][col][0] += grid[row][col+1][1]
-            grid[row][col][0] -= 4 * grid[row][col][1]
-            grid[row][col][0] *= rho
-            grid[row][col][0] += 2 * grid[row][col][1]
-            grid[row][col][0] -= (1 - eta) * grid[row][col][2]
-            grid[row][col][0] /= (1 + eta)
+  if index == 0:
+    grid[0][0][0] = G * grid[1][0][0]
+  elif index == 1:
+    grid[width-1][0][0] = G * grid[width-2][0][0]
+  elif index == 3:
+    grid[0][width-1][0] = G * grid[0][width-2][0]
+  else:
+    grid[width-1][width-1][0] = G * grid[width-1][width-2][0]
 
-        #boundary nodes need to wait here for the interior to be written
-        cuda.syncthreads()
+@cuda.jit
+def propogate(grid, width):
+  index = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
+  if index >= width * width: return
 
-        thread_is_corner = (row == 0 and col == 0) or (row == 0 and col == width-1) or (row == width-1 and col ==0) or (row == width-1 and col == width-1)
+  row = index // width
+  col = index % width
 
-        #boundary nodes can now start to work
-        if thread_is_boundary and not thread_is_corner:
-            if row == 0:
-                grid[0][col][0] = G * grid[1][col][0]
-            elif row == width - 1:
-                grid[width-1][col][0] = G * grid[width-2][col][0]
-            elif col == 0:
-                grid[row][0][0] = G * grid[row][1][0]
-            elif col == width - 1:
-                grid[row][width-1][0] = G * grid[row][width-2][0]
+  grid[row][col][2] = grid[row][col][1]
+  grid[row][col][1] = grid[row][col][0]
 
-        if (row == 1 and col == 0) or (row == width-2 and col == 0) or (row == 0 and col == width-2) or (row == width-1 and col == width-2):
-            cuda.atomic.add(corner_barrier, 0, -1)
-        
-        if thread_is_corner:
-            #corners will busy-wait until their input data has been written
-            while(corner_barrier[0] != 0):
-                pass
-            cuda.atomic.add(corner_barrier,0,-1)        
+""" MAIN """
 
-            if row == 0 and col ==0:
-                grid[0][0][0] = G * grid[1][0][0]
-            elif row == width-1 and col == 0:
-                grid[width-1][0][0] = G * grid[width-2][0][0]
-            elif row == 0 and col == width-1:
-                grid[0][width-1][0] = G * grid[0][width-2][0]
-            elif row == width-1 and col == width-1:
-                grid[width-1][width-1][0] = G * grid[width-1][width-2][0]
+#decide how to allocate threads/blocks
+num_blocks = 1
+threads_per_block = num_threads
+max_threads_per_block = 32
 
-        #all threads can now propogate their values back
-        grid[row][col][2] = grid[row][col][1]
-        grid[row][col][1] = grid[row][col][0]
-        grid[row][col][0] = grid[row][col][0]
+while threads_per_block > max_threads_per_block:
+    num_blocks += 1
+    threads_per_block = math.ceil(float(num_threads) / float(num_blocks))
 
-        if row == width // 2 and col == width // 2:
-            print(grid[row][col][0])
-            while(cuda.atomic.compare_and_swap(corner_barrier, -4, 4) == 4):
-                pass
+    #check if we're using too many blocks
+    if(num_blocks > 65535):
+        num_blocks = 1
+        threads_per_block = num_threads
+        max_threads_per_block *= 2
 
-        cuda.syncthreads()
+#make the grid
+grid = np.zeros((grid_size, grid_size, 3), dtype=np.float)
+grid[grid_size//2][grid_size//2][1] = np.float(1.0)
+grid_d = cuda.to_device(grid)
 
-        q1.print_grid(grid)
+tmp = np.array([0.0], dtype=np.float)
+output = cuda.device_array_like(tmp)
 
-        iteration += 1
+num_iterations = 3
+if len(sys.argv) >= 2:
+    num_iterations = int(sys.argv[1])
 
+for i in range(num_iterations):
+  process_interior[num_blocks,threads_per_block](grid_d, output, grid_size)
+  process_edges[num_blocks,threads_per_block](grid_d, grid_size)
+  process_corners[num_blocks,threads_per_block](grid_d, grid_size)
+  propogate[num_blocks,threads_per_block](grid_d, grid_size)
+  #print(grid_d.copy_to_host())
+  result = output.copy_to_host()
+  
+  print(result[0].round(6))
 
-if __name__ == "__main__":
-    # initialize the grid
-    grid = np.zeros((grid_size, grid_size, 3), dtype=np.float)
-    grid[grid_size//2,grid_size//2,1] = 1
-
-    #get CLAs
-    num_iterations = 3
-    if len(sys.argv) >= 2:
-        num_iterations = int(sys.argv[1])
-
-    barrier = np.array([4])
-    barrier_d = cuda.to_device(barrier)
-    grid_d = cuda.to_device(grid)
-
-    iterate[1,grid_size * grid_size](grid_d, np.int(grid_size), barrier_d, np.int(num_iterations))
-
-
-    
-    
-
+  
